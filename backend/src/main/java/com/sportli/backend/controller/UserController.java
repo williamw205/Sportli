e06df407d0bf2799135c9a7f13224957f607fe86
William Wong <williamw205@gmail.com>
2025-05-12 11:18:37 -0700

    Created model, controller, repository, and service for user profile

---
+++ backend/src/main/java/com/sportli/backend/controller/UserController.java
@@ -0,0 +1,5 @@
+package com.sportli.backend.controller;
+
+public class UserController {
+
+}
---
+++ backend/src/main/java/com/sportli/backend/model/User.java
@@ -0,0 +1,30 @@
+package com.sportli.backend.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String email;
+    private String name;
+    private String sports;
+    private String skillLevel;
+    private String availability;
+    private String bio;
+    private Boolean isEduVerified;
+}
