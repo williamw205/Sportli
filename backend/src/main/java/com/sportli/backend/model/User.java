194810a7e3e7f6fa39b0fb2a4f28feaf36f9d429
William Wong <williamw205@gmail.com>
2025-05-14 15:50:23 -0700

    Fixed spring env library and updated application property

--- backend/src/main/java/com/sportli/backend/model/User.java
+++ backend/src/main/java/com/sportli/backend/model/User.java
@@ -4,14 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
-import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.Data;
 
 @Entity
-@Getter
-@Setter
+@Data
+@Table(name = "users")
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
